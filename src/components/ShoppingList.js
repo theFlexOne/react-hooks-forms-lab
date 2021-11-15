6461ae4b612fa8f64113015153170d3608bb34f8
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2021-11-14 23:52:09 -0600

    assignment complete

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,27 +1,45 @@
-import React, { useState } from "react";
-import ItemForm from "./ItemForm";
-import Filter from "./Filter";
-import Item from "./Item";
+import React, { useState } from 'react';
+import ItemForm from './ItemForm';
+import Filter from './Filter';
+import Item from './Item';
 
 function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [currentItems, setCurrentItems] = useState(items);
+  const [selectedCategory, setSelectedCategory] = useState('All');
+  const [searchFilter, setSearchFilter] = useState('');
 
-  function handleCategoryChange(event) {
+  const handleCategoryChange = event => {
     setSelectedCategory(event.target.value);
-  }
+  };
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  const handleSearchChange = e => {
+    setSearchFilter(e.target.value);
+  };
 
-    return item.category === selectedCategory;
+  const handleItemFormSubmit = newItem => {
+    setCurrentItems([...currentItems, newItem]);
+  };
+
+  const itemsToDisplay = currentItems.filter(item => {
+    if (selectedCategory === 'All' && searchFilter === '') return true;
+    if (selectedCategory === 'All')
+      return item.name.toLowerCase().includes(searchFilter.toLowerCase());
+    return (
+      item.category === selectedCategory &&
+      item.name.toLowerCase().includes(searchFilter.toLowerCase())
+    );
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={handleItemFormSubmit} />
+      <Filter
+        search={searchFilter}
+        onCategoryChange={handleCategoryChange}
+        onSearchChange={handleSearchChange}
+      />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {itemsToDisplay.map(item => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
